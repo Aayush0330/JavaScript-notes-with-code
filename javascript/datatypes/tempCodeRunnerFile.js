18b98e665ee292b7f70269a53fdd8ba019d2e234
Aayush0330200 <aayushchandak1011@gmail.com>
2025-02-01 16:20:51 +0530

    js

---
+++ javascript/datatypes/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+const heros = ["shaktiman", "naagraj", "doga"]; // array
+
+
+// let myObj = {                           // boject
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world"); // function
+// }
